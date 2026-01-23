3798e2b0ed84710b41260bd661ada9d11aa9c9bc
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-01-23 20:01:46 -0300

    feat: criar um script para buscar valores do view_cards

---
+++ DIO/Banco-de-dados/e-cards/db-scripts/views/select_view_cards.sql
@@ -0,0 +1,17 @@
+SELECT 
+    id,
+    hp,
+    name,
+    info,
+    attack,
+    damage,
+    weak,
+    resistance,
+    retreat,
+    cardNumberInCollection,
+    collection_name,
+    collection_release_date,
+    collection_total_cards,
+    type_name,
+    stage_name
+FROM vw_cards_detalhado;
